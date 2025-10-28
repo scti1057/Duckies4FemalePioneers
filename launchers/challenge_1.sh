566607e16531e459c604c521b9898b35da131523
duckie town <duckie.town@web.de>
2025-10-28 17:26:10 +0100

    New path names

--- launchers/challenge_1.sh
+++ launchers/challenge_1.sh
@@ -2,8 +2,8 @@
 source /environment.sh
 dt-launchfile-init
 
-dt-exec python3 "$DT_REPO_PATH/packages/Challenge_1/src/read_key.py" &
-dt-exec python3 "$DT_REPO_PATH/packages/Challenge_1/src/change_speed.py" & 
-dt-exec python3 "$DT_REPO_PATH/packages/Challenge_1/src/crtl_arrow_keys.py"
+dt-exec python3 "$DT_REPO_PATH/packages/challenge_1/src/read_key.py" &
+dt-exec python3 "$DT_REPO_PATH/packages/challenge_1/src/change_speed.py" & 
+dt-exec python3 "$DT_REPO_PATH/packages/challenge_1/src/crtl_arrow_keys.py"
 
 dt-launchfile-join

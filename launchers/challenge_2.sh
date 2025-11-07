4b10ac93c85548a2dfc6713f0dc9f59d6756f42f
ilham-9-9 <ilham.hank9@gmail.com>
2025-11-07 15:05:55 +0100

    now calls not the solution anymore

--- launchers/challenge_2.sh
+++ launchers/challenge_2.sh
@@ -6,8 +6,8 @@ source /environment.sh
 dt-launchfile-init
 
 # launch subscriber
-dt-exec python3 "$DT_REPO_PATH/packages/challenge_2_solution/src/CameraNode.py" &
-dt-exec python3 "$DT_REPO_PATH/packages/challenge_2_solution/src/HardCodedDriveNode.py"
+dt-exec python3 "$DT_REPO_PATH/packages/challenge_2/src/CameraNode.py" &
+dt-exec python3 "$DT_REPO_PATH/packages/challenge_2/src/HardCodedDriveNode.py"
 
 # wait for app to end
 dt-launchfile-join
